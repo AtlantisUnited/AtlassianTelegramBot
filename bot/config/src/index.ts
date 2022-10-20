919f13b6e90f08a54d3b0ca02e29ebe977e75328
Nikita Elfimov <me@tfk.name>
2022-10-21 00:12:25 +0500

    style: message format

--- bot/config/src/index.ts
+++ bot/config/src/index.ts
@@ -1,13 +1,50 @@
 const path = require('path')
 
+// eslint-disable-next-line no-shadow
+export enum PriorityName {
+  LOWEST,
+  LOW,
+  MEDIUM,
+  HIGH,
+  HIGHEST,
+  BLOCKING,
+}
+
+export class Priority {
+  #id!: number
+
+  #priority!: PriorityName
+
+  constructor(id: number, priority: PriorityName) {
+    this.#id = id
+    this.#priority = priority
+  }
+
+  get id() {
+    return this.#id
+  }
+
+  get priority() {
+    return this.#priority
+  }
+}
+
 export default {
   port: 7777,
   bots: [
     {
       routeName: 'jira',
-      channelId: '-1001018013852',
+      channelId: '1068803347',
       templatesPath: path.join(__dirname, '../../Templates', 'JIRA', 'src'),
-      token: '290491673:AAEUvxQ8twy6owhgct-COo7OkcO8KfgfTuA',
+      token: '5009195005:AAHrELxODmqALkTlbTaMUYgH3LNBgrBYxKI',
+      priorities: [
+        new Priority(1, PriorityName.HIGHEST),
+        new Priority(2, PriorityName.HIGH),
+        new Priority(3, PriorityName.MEDIUM),
+        new Priority(4, PriorityName.LOW),
+        new Priority(5, PriorityName.LOWEST),
+        new Priority(6, PriorityName.BLOCKING),
+      ],
     },
     {
       routeName: 'confluence',
